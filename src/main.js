9babc544d5f32e914935c2e3a3bd98ef8a8b3ed1
Leonardo Vilarinho <leonardo-i@outlook.com>
2017-10-23 14:38:02 +0000

    remove vue-router

--- src/main.js
+++ src/main.js
@@ -1,28 +1,7 @@
 import Vue from 'vue'
-// import VueRouter from 'vue-router'
-
 import App from './App.vue'
-// import Painel from './componentes/Painel.vue'
-// import ProdutoLista from './componentes/ProdutoLista.vue'
-// import ProdutoNovo from './componentes/ProdutoNovo.vue'
-
-// Vue.use(VueRouter)
-
-// const router = new VueRouter({
-//   routes: [
-//     { path: '/', component: Painel },
-//     {
-//       path: '/produtos',
-//       component: ProdutoLista,
-//       children: [
-//         { path: 'novo', component: ProdutoNovo }
-//       ]
-//     },
-//   ]
-// })
 
 new Vue({
   el: '#app',
   render: h => h(App),
-  // router: router
 })
